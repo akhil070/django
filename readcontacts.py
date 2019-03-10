14568455ab7e635a0a178881dcf0620202df3c82
akhil <akhilrao199867@gmail.com>
2019-03-10 08:27:21 +0530

    simple email system

---
+++ readcontacts.py
@@ -0,0 +1,77 @@
+import smtplib,os
+
+from string import Template
+from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+MY_ADDRESS = 'cvr.it2016@gmail.com'
+PASSWORD = '16b81aabcd'
+
+def get_contacts(filename):
+    """
+    Return two lists names, emails containing names and email addresses
+    read from a file specified by filename.
+    """
+    
+    names = []
+    emails = []
+    with open(filename, mode='r', encoding='utf-8') as contacts_file:
+        for a_contact in contacts_file:
+            names.append(a_contact.split()[0])
+            emails.append(a_contact.split()[1])
+    return names, emails
+
+def read_template(filename):
+    """
+    Returns a Template object comprising the contents of the 
+    file specified by filename.
+    """
+    
+    with open(filename, 'r', encoding='utf-8') as template_file:
+        template_file_content = template_file.read()
+    return Template(template_file_content)
+
+def read_image(Image):
+    img_data = open(Image, 'rb').read()
+    image = MIMEImage(img_data, name=os.path.basename(Image))
+    return image 
+
+def main():
+    names, emails = get_contacts('mycontacts.txt') # read contacts
+    message_template = read_template('message.txt') # read messages
+    image = read_image('monkey.jpg') #reading image from file
+
+    # set up the SMTP server
+    s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    s.starttls()
+    s.login(MY_ADDRESS, PASSWORD)
+
+    # For each contact, send the email:
+    for name, email in zip(names, emails):
+        msg = MIMEMultipart()       # create a message
+
+        # add in the actual person name to the message template
+        message = message_template.substitute(PERSON_NAME=name.title())
+
+        # Prints out the message body for our sake
+        print(message)
+
+        # setup the parameters of the message
+        msg['From']=MY_ADDRESS
+        msg['To']=email
+        msg['Subject']="This is TEST Email sending from python"
+        
+        # add in the message body
+        msg.attach(MIMEText(message, 'plain'))
+        msg.attach(image)
+        # send the message via the server set up earlier.
+        s.send_message(msg)
+        del(msg)
+        
+    # Terminate the SMTP session and close the connection
+    s.quit()
+    
+if __name__ == '__main__':
+    main()
